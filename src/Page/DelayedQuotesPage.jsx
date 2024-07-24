04cd8a8f9b9e507bd049c392d7f88cceaabd0103
movsic08 <lejero08@gmail.com>
2024-07-24 13:33:13 +0800

    FrontEnd: Flash working initial to vol and last only (INITIAL FLASH)

--- src/Page/DelayedQuotesPage.jsx
+++ src/Page/DelayedQuotesPage.jsx
@@ -14,6 +14,7 @@ export default function DelayedQuotesPage() {
     setIsLoading(true);
     setTableContent(num);
   };
+
   const changeMarket = (num) => {
     setIsLoading(true);
     setMarketId(num);
@@ -25,41 +26,38 @@ export default function DelayedQuotesPage() {
         const response = await axios.get(
           `https://livefeed3.chartnexus.com/Dummy/quotes?market_id=${marketId}&list=${tableContent}`
         );
-        console.log(
-          `      https://livefeed3.chartnexus.com/Dummy/quotes?market_id=${marketId}&list=${tableContent}`
-        );
-
         const newData = response.data;
-        const hasChanged = newData.some((item, index) => {
-          const prevItem = prevDataRef.current[index];
-          return (
-            prevItem &&
-            (item.last !== prevItem.last ||
-              item.volume !== prevItem.volume ||
-              item.buy_price !== prevItem.buy_price ||
-              item.sell_price !== prevItem.sell_price)
-          );
-        });
-        if (hasChanged) {
-          setData(newData);
-        } else {
-          setData(newData);
-        }
 
-        prevDataRef.current = newData;
+        // Determine if there are changes compared to the previous data
+        const updatedData = newData.map((item, index) => {
+          const prevItem = prevDataRef.current[index];
+          return {
+            ...item,
+            hasChanged: prevItem
+              ? item.last !== prevItem.last ||
+                item.volume !== prevItem.volume ||
+                item.buy_price !== prevItem.buy_price ||
+                item.sell_price !== prevItem.sell_price
+              : true,
+          };
+        });
+        console.log(
+          ` https://livefeed3.chartnexus.com/Dummy/quotes?market_id=${marketId}&list=${tableContent}`
+        );
         console.log(response.data ? "fetched success" : "not fetched");
-        setData(response.data);
+
+        setData(updatedData);
+        prevDataRef.current = newData;
         setIsLoading(false);
       } catch (error) {
         console.log("Error fetching data: ", error);
         setIsLoading(false);
       }
     };
-    // Fetch data initially and set up interval
+
     fetchData();
     const interval = setInterval(fetchData, 5000);
 
-    // Clear interval on component unmount
     return () => clearInterval(interval);
   }, [tableContent, marketId]);
 
@@ -74,18 +72,18 @@ export default function DelayedQuotesPage() {
   };
 
   const percentageChange = (last, prev) => {
-    return (100 * (last - prev)) / prev;
+    return prev !== 0 ? (100 * (last - prev)) / prev : 0;
   };
 
   return (
     <>
-      <div className="overflow-x-auto  bg-white drop-shadow-md  dark:bg-gray-800">
+      <div className="overflow-x-auto bg-white drop-shadow-md dark:bg-gray-800">
         <div className="w-full flex items-center justify-between py-2 dark:bg-gray-700 bg-gray-100 px-4">
-          <h1 className=" text-xl font-bold">DELAYED QUOTES</h1>
+          <h1 className="text-xl font-bold">DELAYED QUOTES</h1>
           <div>
             <select
               onChange={(e) => changeMarket(e.target.value)}
-              className="bg-gray-50 border w-fit border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border w-fit border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               <option value="0" defaultValue>
                 SGX
@@ -99,9 +97,9 @@ export default function DelayedQuotesPage() {
           <div
             onClick={() => changeTable(0)}
             className={`border-r-2 px-8 cursor-pointer text-right w-full border-b-2 ${
-              tableContent == 0
+              tableContent === 0
                 ? "border-blue-600 text-blue-600"
-                : " border-slate-200"
+                : "border-slate-200"
             }`}
           >
             Top <br /> Volume
@@ -109,9 +107,9 @@ export default function DelayedQuotesPage() {
           <div
             onClick={() => changeTable(1)}
             className={`border-r-2 px-8 cursor-pointer text-right w-full border-b-2 ${
-              tableContent == 1
+              tableContent === 1
                 ? "border-blue-600 text-blue-600"
-                : " border-slate-200"
+                : "border-slate-200"
             }`}
           >
             Top <br /> Gainers
@@ -119,9 +117,9 @@ export default function DelayedQuotesPage() {
           <div
             onClick={() => changeTable(2)}
             className={`border-r-2 px-8 cursor-pointer text-right w-full border-b-2 ${
-              tableContent == 2
+              tableContent === 2
                 ? "border-blue-600 text-blue-600"
-                : " border-slate-200"
+                : "border-slate-200"
             }`}
           >
             Top <br /> Losers
@@ -129,45 +127,45 @@ export default function DelayedQuotesPage() {
           <div
             onClick={() => changeTable(3)}
             className={`border-r-2 px-8 cursor-pointer text-right w-full border-b-2 ${
-              tableContent == 3
+              tableContent === 3
                 ? "border-blue-600 text-blue-600"
-                : " border-slate-200"
+                : "border-slate-200"
             }`}
           >
             Top % <br /> Gainer
           </div>
           <div
             onClick={() => changeTable(4)}
-            className={` px-8 text-right cursor-pointer w-full border-b-2 ${
-              tableContent == 4
+            className={`px-8 text-right cursor-pointer w-full border-b-2 ${
+              tableContent === 4
                 ? "border-blue-600 text-blue-600"
-                : " border-slate-200"
+                : "border-slate-200"
             }`}
           >
             Top % <br /> Loser
           </div>
         </div>
-        <table className="min-w-full ">
+        <table className="min-w-full">
           <thead>
-            <tr className=" text-slate-500 border-b-[1px] border-gray-600">
+            <tr className="text-slate-500 border-b-[1px] border-gray-600">
               <th className="px-4 pt-2 dark:text-slate-300 text-left">
-                <div className=" font-normal">Stock</div>
-                <div className=" font-normal"> Code</div>
+                <div className="font-normal">Stock</div>
+                <div className="font-normal"> Code</div>
               </th>
               <th className="px-4 pt-2 dark:text-slate-300 text-right">
-                <div className=" font-normal">Last</div>
+                <div className="font-normal">Last</div>
                 <div className="font-normal"> Vol</div>
               </th>
               <th className="px-4 pt-2 dark:text-slate-300 text-right">
-                <div className=" font-normal"> +/-</div>
+                <div className="font-normal"> +/-</div>
                 <div className="font-normal"> %Chng</div>
               </th>
               <th className="px-4 pt-2 dark:text-slate-300 text-right">
-                <div className=" font-normal"> Buy</div>
+                <div className="font-normal"> Buy</div>
                 <div className="font-normal"> Buy Vol</div>
               </th>
               <th className="px-4 pt-2 dark:text-slate-300 text-right">
-                <div className=" font-normal"> Sell</div>
+                <div className="font-normal"> Sell</div>
                 <div className="font-normal"> Sell Vol</div>
               </th>
             </tr>
@@ -179,24 +177,31 @@ export default function DelayedQuotesPage() {
               data.map((item, index) => (
                 <tr
                   key={index}
-                  className="hover:bg-gray-100 dark:hover:bg-gray-700 border-b-2 "
+                  className="hover:bg-gray-100 dark:hover:bg-gray-700 border-b-2"
                 >
-                  <td className=" px-4 py-2 text-left">
-                    <strong className="flash">{item.name}</strong>
+                  <td className="px-4 py-2 text-left">
+                    <strong
+                      className={`${
+                        item.hasChanged ? "flash" : ""
+                      } transition-colors duration-500`}
+                    >
+                      {item.name}
+                    </strong>
                     <div>{item.stockcode}</div>
                   </td>
                   <td
-                    className={` px-4 py-2 text-right ${
-                      item.last !== prevDataRef.current[index]?.last ||
-                      item.volume !== prevDataRef.current[index]
-                        ? "flash"
-                        : ""
-                    }`}
+                    className={`px-4 py-2 text-right ${
+                      item.hasChanged ? "flash" : ""
+                    } transition-colors duration-500`}
                   >
                     <div>{item.last.toFixed(3)}</div>
                     <div>{formatMoney(item.volume)}</div>
                   </td>
-                  <td className=" px-4 py-2 text-right flash">
+                  <td
+                    className={`px-4 py-2 text-right ${
+                      item.hasChanged ? "flash" : ""
+                    } transition-colors duration-500`}
+                  >
                     <div
                       className={
                         item.last - item.previous < 0
@@ -206,7 +211,6 @@ export default function DelayedQuotesPage() {
                     >
                       {(item.last - item.previous).toFixed(3)}
                     </div>
-
                     <div
                       className={
                         percentageChange(item.last, item.previous) < 0
@@ -217,11 +221,11 @@ export default function DelayedQuotesPage() {
                       {percentageChange(item.last, item.previous).toFixed(1)}%
                     </div>
                   </td>
-                  <td className=" px-4 py-2 text-right">
+                  <td className="px-4 py-2 text-right">
                     <div>{item.buy_price.toFixed(3)}</div>
                     <div>{formatMoney(item.buy_volume)}</div>
                   </td>
-                  <td className=" px-4 py-2 text-right">
+                  <td className="px-4 py-2 text-right">
                     <div>{item.sell_price.toFixed(3)}</div>
                     <div>{formatMoney(item.sell_volume)}</div>
                   </td>
